e40521aa31d8d0527112c139d00fc5f477f44c1f
Lufrauwu <lexius002@gmail.com>
2021-06-09 09:59:07 -0500

    Luis

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World\n";
+    cout << "Buenas tardes ";
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
